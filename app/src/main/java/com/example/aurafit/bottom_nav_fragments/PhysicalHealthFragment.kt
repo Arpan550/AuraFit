5cc3f4a46923ba9de6f36691f4760e57f71a5564
Arpan550 <arpanbhattacharya5356@gmail.com>
2024-06-24 20:11:50 +0530

    @updated

--- app/src/main/java/com/example/aurafit/bottom_nav_fragments/PhysicalHealthFragment.kt
+++ app/src/main/java/com/example/aurafit/bottom_nav_fragments/PhysicalHealthFragment.kt
@@ -1,21 +1,39 @@
 package com.example.aurafit.bottom_nav_fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.aurafit.R
-
+import com.example.aurafit.MusicActivity
+import com.example.aurafit.databinding.FragmentPhysicalHealthBinding
 
 class PhysicalHealthFragment : Fragment() {
+
+    private var physicalbinding: FragmentPhysicalHealthBinding? = null
+    private val binding get() = physicalbinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_physical_health, container, false)
+        physicalbinding = FragmentPhysicalHealthBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Handle click event for the music card
+        binding.musicCont.setOnClickListener {
+            val intent = Intent(activity, MusicActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        physicalbinding = null
+    }
 }
